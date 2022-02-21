69c8444289799743ff3568f2eab6704988619f70
Juana-89 <juana.lc@hotmail.es>
2022-02-21 11:23:52 -0500

    Updates

---
+++ cipher.js
@@ -0,0 +1,32 @@
+
+   const cipher = {
+      encode: (offset, string) => {
+      let result = "";
+      for (let i = 0; i < string.length; i++) {
+          let codeAscii = string.charCodeAt(i);
+          if (codeAscii >= 65 && codeAscii <=90) {
+            result += String.fromCharCode(((codeAscii - 65 + offset) % 26) + 65);
+          } /*else if (codeAscii >= 97 && codeAscii <=122) {
+            result += String.fromCharCode(((codeAscii - 97 + offset) % 26) + 97);
+
+       }*/
+      }
+           //console.log(result);
+            return result;
+  },
+
+      decode: (offset, string) => {
+      let result = "";
+      for (let i = 0; i < string.length; i++) {
+          let codeAscii = string.charCodeAt(i);
+          if (codeAscii >= 65 && codeAscii <=90) {
+            result += String.fromCharCode(((codeAscii + 65 - offset) % 26) + 65);
+           } /* else if (codeAscii >= 97 && codeAscii <=122) {
+            result += String.fromCharCode(((codeAscii + 97 - offset) % 26) + 97);
+     } */
+     }
+   //  console.log(result);
+            return result;
+    }
+};
+export default cipher;
